92b54828e8c57fd334e4c0954d0ad901c21c2cc1
Richy <richy@webcomm.com.tw>
2023-07-18 17:02:13 +0800

    new UIButton, set property屬性（系統, 圖片按鈕）, show button

--- HelloUIButton/ViewController.swift
+++ HelloUIButton/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         print("Hello World")
     }
     
+    // 宣告觸發事件 function
     @objc func hitMe(_ thisButton:UIButton) { // @objc 讓 objective-c 可以呼叫
         print("Yo! What's up ??")
     }
@@ -35,21 +36,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // how to make system style button via code
+        // how to make system style button via code // new UIButton
         let newButton = UIButton(type: .system)
-        newButton.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
-        newButton.setTitle("Press", for: .normal)
-        newButton.setTitle("Pressing", for: .highlighted)
-        newButton.addTarget(self, action: #selector(ViewController.hitMe(_:)), for: .touchUpInside)
-        view.addSubview(newButton)
+        newButton.frame = CGRect(x: 50, y: 50, width: 100, height: 50) // 位置大小
+        newButton.setTitle("Press", for: .normal) // 未點擊時文字
+        newButton.setTitle("Pressing", for: .highlighted) // 點擊時文字
+        newButton.addTarget(self, action: #selector(ViewController.hitMe(_:)), for: .touchUpInside) // 點擊時觸發事件
+        view.addSubview(newButton) // show到畫面上
         
         // how to make image button via code
         let anotherButton = UIButton(type: .custom)
         anotherButton.frame = CGRect(x: 200, y: 200, width: 114, height: 54)
-        anotherButton.setImage(UIImage(named: "PlayButton"), for: .normal)
-        anotherButton.setImage(UIImage(named: "PlayButtonPressed"), for: .highlighted)
+        anotherButton.setImage(UIImage(named: "PlayButton"), for: .normal) // 按鈕圖片 未點擊時
+        anotherButton.setImage(UIImage(named: "PlayButtonPressed"), for: .highlighted) // 按鈕圖片 點擊時
         anotherButton.addTarget(self, action: #selector(ViewController.hitMe(_:)), for: .touchUpInside)
-        view.addSubview(anotherButton)
+        view.addSubview(anotherButton) // show到畫面上
     }
 
 
